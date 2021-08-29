0039d6e2221a50f0ec2dca8bd613224ae12034c2
Andrei Bissolotti <andreibisso@gmail.com>
2021-08-29 15:47:07 -0300

    getElementsByClassName

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // getElementById()
 
-console.log(document.getElementById('blog-title'))
+const element1 = document.getElementById('blog-title')
+console.log(element1)
+
+
+// getElementsByClassName
+
+const element2 = document.getElementsByClassName('one')
+console.log(element2)
