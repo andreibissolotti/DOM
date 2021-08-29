59f0b7cda9b3e68513215fa3172ef280bc682b97
Andrei Bissolotti <andreibisso@gmail.com>
2021-08-29 15:51:05 -0300

    getElementsByTagName

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// getElementById()
+// getElementById('Id do elemento')
 
 const element1 = document.getElementById('blog-title')
 console.log(element1)
 
 
-// getElementsByClassName
+// getElementsByClassName('nome da classe')
 
 const element2 = document.getElementsByClassName('one')
 console.log(element2)
+
+// getElementsByTagName('nome da tag')
+
+element3 = document.getElementsByTagName('meta')
+console.log(element3)
