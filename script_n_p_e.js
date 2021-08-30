84ff84f5e8a76714217b026d52cf1c921395b604
Andrei Bissolotti <andreibisso@gmail.com>
2021-08-30 16:36:49 -0300

    childNode & children

--- script_n_p_e.js
+++ script_n_p_e.js
@@ -4,3 +4,13 @@
 const element = document.querySelector('h1')
 
 console.log(element.parentNode)
+
+// childNodes (considera os espaços vazios) children (ignora espaços vazios)
+const el = document.querySelector('body')
+
+console.log(el.children)
+
+// firstChild firstElementChild
+
+
+// lastChild lastElementChild
