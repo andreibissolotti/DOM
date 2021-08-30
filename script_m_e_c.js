97680650c104eb560115986708f403dc6ab8993f
Andrei Bissolotti <andreibisso@gmail.com>
2021-08-30 16:02:54 -0300

    classList

--- script_m_e_c.js
+++ script_m_e_c.js
@@ -1,4 +1,14 @@
 // Alterando estilos
+
+// style
 const element = document.querySelector('body')
 
-element.style.backgroundColor = "#f9f3D2"
+//element.style.backgroundColor = "#f9f3D2"
+
+// classList
+
+element.classList.add('active', 'green')
+
+console.log(element.classList)
+element.classList.remove('active')
+element.classList.toggle('active') // (pesquisa pela clase, se existir, remove, se não, adiciona)
