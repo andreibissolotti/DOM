740157843359eb50195ec6ebf1c881d29ef6e8f4
Andrei Bissolotti <andreibisso@gmail.com>
2021-08-30 10:42:33 -0300

    innerHTML

--- script_m_c.js
+++ script_m_c.js
@@ -1,14 +1,16 @@
 // Manipulando conteúdo
 
-// textContent
+// textContent (retorna conteudos ocultos pelo css)
 const element = document.querySelector('h1')
 
 element.textContent = "Olá Devs!"
 
 console.log(element.textContent)
 
-// innerText
+// innerText (não retorna conteudos ocultos pelo css)
 
 element.innerText= "Olá Devs!"
 
-// 
+// innerHTML
+
+element.innerHTML= "Olá Devs! <small>!!!</small>"
